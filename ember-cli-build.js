1128269a8b72505afe0ac3c4b654cd0b9c70b7cd
The-Foible <xander.doom@gmail.com>
2024-09-06 09:54:21 -0400

    Fix chrome bug

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    autoImport: {
+      forbidEval: true,
+    },
   });
 
   app.import('node_modules/todomvc-common/base.css');
